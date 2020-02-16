f77038499ee3727a88157913fc4247ad71deb151
Angelo Vicente Filho <angelovicentefilho@gmail.com>
2020-02-16 11:51:08 -0300

    Initial commit

---
+++ src/main/java/br/com/jwt/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package br.com.jwt.filters;
+
+import static br.com.jwt.constants.SecurityConstants.AUTH_LOGIN_URL;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.jwt.constants.SecurityConstants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private final AuthenticationManager manager;
+
+	public JwtAuthenticationFilter(AuthenticationManager manager) {
+		this.manager = manager;
+		setFilterProcessesUrl(AUTH_LOGIN_URL);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+				password);
+
+		return manager.authenticate(authenticationToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain, Authentication authentication) {
+		User user = ((User) authentication.getPrincipal());
+
+		List<String> roles = user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+				.collect(Collectors.toList());
+
+		byte[] signingKey = SecurityConstants.JWT_SECRET.getBytes();
+
+		String token = Jwts.builder().signWith(Keys.hmacShaKeyFor(signingKey), SignatureAlgorithm.HS512)
+				.setHeaderParam("typ", SecurityConstants.TOKEN_TYPE).setIssuer(SecurityConstants.TOKEN_ISSUER)
+				.setAudience(SecurityConstants.TOKEN_AUDIENCE).setSubject(user.getUsername())
+				.setExpiration(new Date(System.currentTimeMillis() + 864000000)).claim("rol", roles).compact();
+
+		response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX + token);
+	}
+
+}
